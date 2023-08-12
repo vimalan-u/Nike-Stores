dc0139e978b0a9806e2be188cff553b5075229fd
VenketeshRushi <rushivenketesh@gmail.com>
2023-08-12 13:10:23 +0530

    updated routes to handel 404 error of github pages

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/ecom-client">
+        <BrowserRouter>
           <ChakraProvider theme={theme}>
             <App />
           </ChakraProvider>
