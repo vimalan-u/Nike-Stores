302bd212043cc0bcd82304dc32247b5733a3fed6
Raman <rushivenketesh@gmail.com>
2023-08-22 22:57:05 +0530

    Fixed issue in Allorders page

--- src/utils/dateFormator.js
+++ src/utils/dateFormator.js
@@ -10,3 +10,19 @@ export const dateFormator = (dt) => {
 
     return { date, time };
 };
+
+export const dateFormatorAllOrder = (dt) => {
+    const rawDate = new Date(dt);
+  
+    const time = rawDate.toLocaleTimeString();
+  
+    const dateFormatter = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  
+    const date = dateFormatter.format(rawDate);
+  
+    return { date, time };
+  };
