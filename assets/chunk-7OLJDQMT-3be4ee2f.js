c2211876b390687c7265b4ce8bfbc1b479179f99
VenketeshRushi <rushivenketesh@gmail.com>
2023-09-06 12:16:53 +0000

    Updates

--- assets/chunk-7OLJDQMT-3be4ee2f.js
+++
@@ -1,1 +0,0 @@
-import{k as i,D as t,o as r,j as c,m,q as o}from"./index-6dab30cb.js";var d=i(function(a,s){const e=t("Heading",a),{className:l,...n}=r(a);return c.jsx(m.h2,{ref:s,className:o("chakra-heading",a.className),...n,__css:e})});d.displayName="Heading";export{d as H};
--- assets/chunk-NTCQBYKE-77819ca8.js
+++
@@ -1,1 +0,0 @@
-import{S as t}from"./chunk-ZHMYA64R-5ef6af8e.js";import{k as o,j as s}from"./index-6dab30cb.js";var e=o((a,r)=>s.jsx(t,{align:"center",...a,direction:"column",ref:r}));e.displayName="VStack";export{e as V};
