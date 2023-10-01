41c703e129dbfe6d20ba24aad5dd2d22d4afe1d0
Raman <rushivenketesh@gmail.com>
2023-10-01 12:22:00 +0530

    fixed order chart issue in admin dashboard page

--- src/srcADMIN/pages/Dashboard.admin.jsx
+++ src/srcADMIN/pages/Dashboard.admin.jsx
@@ -1,11 +1,7 @@
-import OrderChart from "../components/dashboard/OrderChart";
-
-
 function Dashboard() {
   return (
     <>
       <h1>Welcome To Admin Dashboard.</h1>
-      <OrderChart />
     </>
   );
 }
