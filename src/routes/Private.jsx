3458883e7daf6f8bfee05d56ded502989cbb698d
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-09 10:32:43 +0530

    updated login route

--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -4,12 +4,12 @@ import { Navigate } from "react-router-dom";
 export const Private = ({ children }) => {
   const token = useSelector((state) => state.auth.token);
 
-  return token.length > 0 ? children : <Navigate to={"/auth"} />;
+  return token.length > 0 ? children : <Navigate to={"/login"} />;
 };
 
 export const PrivateAdmin = ({ children }) => {
   const token = useSelector((state) => state.auth.token);
   const isAdmin = useSelector((state) => state.auth.isAdmin);
 
-  return token.length > 0 && !!isAdmin ? children : <Navigate to={"/auth"} />;
+  return token.length > 0 && !!isAdmin ? children : <Navigate to={"/login"} />;
 };
