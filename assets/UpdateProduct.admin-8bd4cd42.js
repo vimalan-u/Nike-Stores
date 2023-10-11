180c8248d561e3155cc6628d19fb1e27eb4747de
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-11 15:54:00 +0000

    Updates

---
+++ assets/UpdateProduct.admin-8bd4cd42.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-8e354a13.js";function e(){return t.jsx("h1",{children:"Update Product"})}export{e as default};
--- assets/chunk-7OLJDQMT-38a17553.js
+++
@@ -1,1 +0,0 @@
-import{t,a7 as i,a8 as r,j as c,v as m,w as o}from"./index-fa79aa71.js";var d=t(function(a,s){const e=i("Heading",a),{className:l,...n}=r(a);return c.jsx(m.h2,{ref:s,className:o("chakra-heading",a.className),...n,__css:e})});d.displayName="Heading";export{d as H};
