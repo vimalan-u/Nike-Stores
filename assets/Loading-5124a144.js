4e77adfb862954140e97279a3e6ee9373225a4a3
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-22 16:49:37 +0000

    Updates

--- assets/Loading-5124a144.js
+++
@@ -1,1 +0,0 @@
-import{j as t,aw as e}from"./index-94144462.js";import{C as n}from"./chunk-5MKCW436-9a270bd1.js";const i=()=>t.jsx(n,{my:"140px",display:"flex",justifyContent:"center",height:"30vh",children:t.jsx(e,{size:"xl"})});export{i as L};
---
+++ assets/Loading-a2795243.js
@@ -0,0 +1,1 @@
+import{j as t,aw as e}from"./index-849325ce.js";import{C as n}from"./chunk-5MKCW436-3a641680.js";const i=()=>t.jsx(n,{my:"140px",display:"flex",justifyContent:"center",height:"30vh",children:t.jsx(e,{size:"xl"})});export{i as L};
