88364d6a7a9a15d68e9f16459a6e7771d9b41505
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-22 15:56:40 +0000

    Updates

--- assets/Loading-6d56e416.js
+++
@@ -1,1 +0,0 @@
-import{j as t,aw as e}from"./index-2da8025d.js";import{C as n}from"./chunk-5MKCW436-6db5b3ac.js";const i=()=>t.jsx(n,{my:"140px",display:"flex",justifyContent:"center",height:"30vh",children:t.jsx(e,{size:"xl"})});export{i as L};
