ab396618d1c4f6a093c022595705755daf499e60
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-23 11:55:17 +0000

    Updates

--- assets/Loading-def7098f.js
+++
@@ -1,1 +0,0 @@
-import{j as t,aw as e}from"./index-7cc77bda.js";import{C as n}from"./chunk-5MKCW436-c2607759.js";const i=()=>t.jsx(n,{my:"140px",display:"flex",justifyContent:"center",height:"30vh",children:t.jsx(e,{size:"xl"})});export{i as L};
