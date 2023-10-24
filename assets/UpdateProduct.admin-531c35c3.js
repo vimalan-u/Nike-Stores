56b3e75c3f78b2249c1af3a4cc7a4875c7d76e41
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-24 15:38:31 +0000

    Updates

--- assets/UpdateProduct.admin-531c35c3.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-4c04a5bf.js";function e(){return t.jsx("h1",{children:"Curretly Working On this Update Product PAge"})}export{e as default};
---
+++ assets/UpdateProduct.admin-61ac9e5a.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fe462ba9.js";function e(){return t.jsx("h1",{children:"Curretly Working On this Update Product PAge"})}export{e as default};
