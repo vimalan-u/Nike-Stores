01f6d4b0d4f5d0ae406193e61aa3117fcca596e2
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-26 17:58:37 +0000

    Updates

--- assets/Loading-16d00587.js
+++
@@ -1,1 +0,0 @@
-import{j as t,aw as e}from"./index-94141324.js";import{C as n}from"./chunk-5MKCW436-fa152346.js";const i=()=>t.jsx(n,{my:"140px",display:"flex",justifyContent:"center",height:"30vh",children:t.jsx(e,{size:"xl"})});export{i as L};
---
+++ assets/Loading-a1d403eb.js
@@ -0,0 +1,1 @@
+import{j as t,aw as e}from"./index-41fe9c67.js";import{C as n}from"./chunk-5MKCW436-89e96c39.js";const i=()=>t.jsx(n,{my:"140px",display:"flex",justifyContent:"center",height:"30vh",children:t.jsx(e,{size:"xl"})});export{i as L};
