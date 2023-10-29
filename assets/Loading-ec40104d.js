3f58a046f28b039da1bb4149d33790f21ad933bb
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-29 09:55:07 +0000

    Updates

---
+++ assets/Loading-ec40104d.js
@@ -0,0 +1,1 @@
+import{j as t,aw as e}from"./index-5e8e1b84.js";import{C as n}from"./chunk-5MKCW436-ab9fc6ab.js";const i=()=>t.jsx(n,{my:"140px",display:"flex",justifyContent:"center",height:"30vh",children:t.jsx(e,{size:"xl"})});export{i as L};
