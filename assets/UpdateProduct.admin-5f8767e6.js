e45de6676830307fa5829de6e5a8c2161c27c6e8
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-30 13:16:38 +0000

    Updates

--- assets/UpdateProduct.admin-5f8767e6.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-19a905a0.js";function e(){return t.jsx("h1",{children:"Curretly Working On this Update Product PAge"})}export{e as default};
---
+++ assets/UpdateProduct.admin-a07413ee.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-d3115749.js";function e(){return t.jsx("h1",{children:"Curretly Working On this Update Product PAge"})}export{e as default};
